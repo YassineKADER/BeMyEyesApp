79e71b2a8412a69601655deb6e8942820d0c6056
EROS <80207770+YassineKADER@users.noreply.github.com>
2024-04-04 23:46:50 +0000

    todo

--- app/(login)/_layout.tsx
+++ app/(login)/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from "expo-router"
 export default function HomeLayout (){
     return (
         <Stack>
+            #TODO Add also the thing related to signup Stack
             <Stack.Screen name="index" options={{headerTitle:'SignIn',headerTransparent:true,headerTitleAlign:'center',headerLeft:()=>null}}/>
         </Stack>
     )
