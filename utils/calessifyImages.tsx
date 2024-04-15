73a0f9d676396927e1be12c57c83ace99ddafadf
EROS <80207770+YassineKADER@users.noreply.github.com>
2024-04-15 15:13:56 +0100

    code clean up

---
+++ utils/calessifyImages.tsx
@@ -0,0 +1,29 @@
+import * as FileSystem from "expo-file-system";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-react-native";
+import { decodeJpeg } from "@tensorflow/tfjs-react-native";
+export async function classifyImage(imgUri: string|null|undefined, model:mobilenet.MobileNet|null|undefined){
+    if(imgUri && model){
+     try {
+      const fileUri = imgUri;
+      const imgB64 = await FileSystem.readAsStringAsync(fileUri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const imgBuffer = tf.util.encodeString(imgB64, "base64").buffer;
+      const newData = new Uint8Array(imgBuffer);
+      const imageTensor = decodeJpeg(newData);
+      const prediction = await model.classify(imageTensor);
+      return prediction
+      
+    } catch (error) {
+      console.log(error);
+      return
+    }
+    }
+    return
+}
+
+
+
+   
---
+++ utils/takePicture.tsx
@@ -0,0 +1,10 @@
+import { Camera, CameraType, PermissionResponse } from "expo-camera";
+import React from "react";
+
+export async function takePicture(camera: React.RefObject<Camera>, permission: PermissionResponse | null) {
+    if (permission && permission.granted === true && camera.current) {
+       let pic = await camera.current.takePictureAsync({quality:0.75, base64: true})
+       return pic.uri; 
+    }
+    return
+}
